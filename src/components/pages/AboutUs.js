a8228f78fae80134e61d9f3feb35ba1ad2906e6b
Adrian-finstad <adrian.finstad@hotmail.com>
2024-08-26 20:51:52 +0200

    removed minor files.

--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -13,39 +13,6 @@ function AboutUs() {
       <TeamSection />
       <Footer />
     </>
-
-    // <>
-
-    //   <div className="about-us-wrapper">
-    //     <div className="animate_animated animate__fadeInUp">
-    //       <h1 className="text">Om Oss</h1>
-    //     </div>
-    //     <div className="about-us-container">
-    //       <div className="image-container flex justify-center">
-    //         <img
-    //           className="about-us-image size-80"
-    //           src="images/Disc_3_Clean.png"
-    //           alt="gps-banner"
-    //           onClick={(event) =>
-    //             window.open("https://www.twitch.tv/sergeantsatanlive", "_blank")
-    //           }
-    //         />
-    //         <img
-    //           className="about-us-image size-80"
-    //           src="images/Jokkimsen.png"
-    //           alt="gps-christmas"
-    //           onClick={(event) =>
-    //             window.open("https://www.twitch.tv/jokkimsen", "_blank")
-    //           }
-    //         />
-    //       </div>
-
-    //       <AboutUsText />
-    //     </div>
-    //   </div>
-
-    //   <Footer />
-    // </>
   );
 }
 
